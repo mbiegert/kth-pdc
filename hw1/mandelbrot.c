33ce238318b06dd8af662fcb1246a264225d39c7
Max Biegert <max@k1te.be>
2019-02-08 17:04:14 +0100

    enable shifting of the viewing area

--- hw1/mandelbrot.c
+++ hw1/mandelbrot.c
@@ -37,12 +37,12 @@
 // the reading flow, starting with main()
 unsigned char calc_mandelbrot_pixel(const double _Complex d, const double bound, const unsigned int num_colors);
 #ifdef ENABLE_PNG
-int write_png_image_to_disk(unsigned char* image, const unsigned int WIDTH,
-                            const unsigned int HEIGHT, const unsigned int NCOLORS,
+int write_png_image_to_disk(unsigned char* image, const unsigned int PIXEL_WIDTH,
+                            const unsigned int PIXEL_HEIGHT, const unsigned int NCOLORS,
                             const char* const filename, const char* const palette);
 #endif
-int write_ascii_image_to_disk(unsigned char* image, const unsigned int WIDTH,
-                            const unsigned int HEIGHT, const unsigned int NCOLORS, const char* const filename);
+int write_ascii_image_to_disk(unsigned char* image, const unsigned int PIXEL_WIDTH,
+                            const unsigned int PIXEL_HEIGHT, const unsigned int NCOLORS, const char* const filename);
 
 enum {
     OUTPUTMODE_ASCII,
@@ -56,22 +56,27 @@ int main(int argc, char **argv) {
     char* outfile_name = "output";
     char* palette_name = "so";
 
-    // global parameters
+    // parameters
     const unsigned int NCOLORS = 256; // Do not change this value. Producing image data
                                       // is only supported for 256 colors.
     const double BOUND = 2.0;
-    const unsigned int HEIGHT = 2048;
-    const unsigned int WIDTH = 2048;
-    const int HOFFSET = 0;
-    const int VOFFSET = 0;
+    const unsigned int PIXEL_HEIGHT = 8192;
+    const unsigned int PIXEL_WIDTH = 8192;
+    // define the viewing area
+    // specify the bottom left coordinate and height, width
+    // default values: -BOUND, -BOUND, 4.0, 4.0
+    const double BOTTOM_LEFT_X = -2.0;
+    const double BOTTOM_LEFT_Y = -1.0;
+    const double IMAGE_HEIGHT = 2.0;
+    const double IMAGE_WIDTH = 2.0;
 
     // steppings
     // We have a viewing area of [-BOUND, BOUND] x [-BOUND, BOUND]
     // and need to calculate a horizontal stepping dx and vertical
     // stepping dy and map this to actual complex numbers in the viewing area
     // (interpret R^2 \cong C)
-    const double dx = 2*BOUND/(double)(WIDTH-1);
-    const double dy = 2*BOUND/(double)(HEIGHT-1);
+    const double dx = IMAGE_WIDTH/(double)(PIXEL_WIDTH-1);
+    const double dy = IMAGE_HEIGHT/(double)(PIXEL_HEIGHT-1);
 
     // initialise and setup MPI
     rc = MPI_Init(&argc, &argv);
@@ -113,17 +118,17 @@ int main(int argc, char **argv) {
     //
     // First determine how many rows we will compute according to the
     // scatter distribution, to allocate the correct amount of memory needed.
-    unsigned int num_rows = HEIGHT / size;
-    const unsigned int rest = HEIGHT % size;
+    unsigned int num_rows = PIXEL_HEIGHT / size;
+    const unsigned int rest = PIXEL_HEIGHT % size;
     // if the number of processes does not divide the number of rows we need
     // to also calculate the remaining rows
     if (rank < rest)
         ++num_rows;
 
     // This array holds the calculated colors and is a contiguous memory area
-    // The first WIDTH uns chars are the first row this process calculated and so on
+    // The first PIXEL_WIDTH uns chars are the first row this process calculated and so on
     // we start from the bottom and work our way upwards.
-    unsigned char *color = (unsigned char*) malloc(WIDTH * num_rows * sizeof(unsigned char));
+    unsigned char *color = (unsigned char*) malloc(PIXEL_WIDTH * num_rows * sizeof(unsigned char));
     if (!color) {
         printf("malloc failed on process of rank %d.\n", rank);
         return 1;
@@ -133,13 +138,13 @@ int main(int argc, char **argv) {
     for (int y = 0; y < num_rows; ++y) {
         // now account for the correct row
         int local_row_offset = rank + (y * size);
-        double dimag = local_row_offset * dy - BOUND + VOFFSET;
+        double dimag = local_row_offset * dy + BOTTOM_LEFT_Y;
 
         // now iterate over all pixels in the row and calculate the color
-        for (int x = 0; x < WIDTH; ++x) {
-            double dreal = x*dx - BOUND + HOFFSET;
+        for (int x = 0; x < PIXEL_WIDTH; ++x) {
+            double dreal = x*dx + BOTTOM_LEFT_X;
             double _Complex d = dreal + dimag * _Complex_I;
-            color[x + y*WIDTH] = calc_mandelbrot_pixel(d, BOUND, NCOLORS);
+            color[x + y*PIXEL_WIDTH] = calc_mandelbrot_pixel(d, BOUND, NCOLORS);
         }
     }
 
@@ -148,9 +153,9 @@ int main(int argc, char **argv) {
         // This is the collector process and we need to gather all the stuff.
 
         // First we allocate another array to hold the whole image
-        // again the first WIDTH unsigned characters correspond to the
+        // again the first PIXEL_WIDTH unsigned characters correspond to the
         // row at the very bottom
-        unsigned char* image = malloc(WIDTH*HEIGHT * sizeof(unsigned char));
+        unsigned char* image = malloc((unsigned long)PIXEL_WIDTH*PIXEL_HEIGHT * sizeof(unsigned char));
         if (!image) {
             printf("Failed to allocate memory for the output image.");
             return 2;
@@ -158,14 +163,14 @@ int main(int argc, char **argv) {
         
         // now loop over all processes and receive their data, start with this process itself
         int received_rank = 0;
-        int received_length = WIDTH*num_rows;
+        unsigned int received_length = PIXEL_WIDTH*num_rows;
         for (int i = 0; i<size; ++i) {
             // only receive and overwrite the color array, if we already copied it
             if (i > 0) {
                 // we receive from any source, the actual sender can later be found in "status"
-                // note also that it is legitimate to use WIDTH*num_rows, since the process 0
+                // note also that it is legitimate to use PIXEL_WIDTH*num_rows, since the process 0
                 // always has the highest number of rows to calculate
-                rc = MPI_Recv(color, WIDTH*num_rows, MPI_UNSIGNED_CHAR,
+                rc = MPI_Recv(color, PIXEL_WIDTH*num_rows, MPI_UNSIGNED_CHAR,
                         MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
                 if (rc) {
                     printf("MPI_Recv failed. Error code: %d\n", rc);
@@ -181,10 +186,12 @@ int main(int argc, char **argv) {
             // array corresponds to one *row*
             //
             // loop over all rows received
-            for (int received_row = 0; received_row * WIDTH < received_length; ++received_row) {
+            for (int received_row = 0; received_row * PIXEL_WIDTH < received_length; ++received_row) {
                 // copy one row to the correct offset in the image array
-                assert(received_rank + received_row * size + WIDTH < WIDTH * HEIGHT);
-                memcpy(image + received_rank + received_row * size * WIDTH, color + received_row * WIDTH, WIDTH * sizeof(unsigned char));
+                assert(received_rank + (unsigned long) received_row * size * PIXEL_WIDTH < (unsigned long) PIXEL_WIDTH * PIXEL_HEIGHT);
+                memcpy(image + received_rank + (unsigned long) received_row * size * PIXEL_WIDTH,
+                        color + (unsigned long)received_row * PIXEL_WIDTH,
+                        PIXEL_WIDTH * sizeof(unsigned char));
             }
         }
 
@@ -193,11 +200,11 @@ int main(int argc, char **argv) {
 
         // now we need to write the image array to disk
         if (output_mode == OUTPUTMODE_ASCII) {
-            rc = write_ascii_image_to_disk(image, WIDTH, HEIGHT, NCOLORS, outfile_name);
+            rc = write_ascii_image_to_disk(image, PIXEL_WIDTH, PIXEL_HEIGHT, NCOLORS, outfile_name);
         }
         #ifdef ENABLE_PNG
         else if (output_mode == OUTPUTMODE_PNG) {
-            rc = write_png_image_to_disk(image, WIDTH, HEIGHT, NCOLORS, outfile_name, palette_name);
+            rc = write_png_image_to_disk(image, PIXEL_WIDTH, PIXEL_HEIGHT, NCOLORS, outfile_name, palette_name);
         }
         #endif
 
@@ -205,7 +212,7 @@ int main(int argc, char **argv) {
     }
     else {
         // If we are not the collector, just send it over to the process of rank 0
-        rc = MPI_Send(color, WIDTH*num_rows, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
+        rc = MPI_Send(color, PIXEL_WIDTH*num_rows, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
         free(color);
     }
 
@@ -226,9 +233,9 @@ unsigned char calc_mandelbrot_pixel(const double _Complex d, const double bound,
     return count-1;
 }
 
-// this function takes an image array of length WIDTH*HEIGHT
+// this function takes an image array of length PIXEL_WIDTH*PIXEL_HEIGHT
 // and produces a file in ascii format of the name <filename>.txt
-int write_ascii_image_to_disk(unsigned char* image, const unsigned int WIDTH, const unsigned int HEIGHT,
+int write_ascii_image_to_disk(unsigned char* image, const unsigned int PIXEL_WIDTH, const unsigned int PIXEL_HEIGHT,
                                 const unsigned int NCOLORS, const char* const filename) {
     char* full_outfile_name = malloc(strlen(filename) + 4);
     strcpy(full_outfile_name, filename);
@@ -240,9 +247,9 @@ int write_ascii_image_to_disk(unsigned char* image, const unsigned int WIDTH, co
     }
 
     // also here reverse rows
-    for (int i = HEIGHT; i > 0; --i) {
-        for (int j = 0; j < WIDTH; ++j)
-            fprintf(fp, "%hhu ", image[j + HEIGHT*(i-1)]);
+    for (unsigned int i = PIXEL_HEIGHT; i > 0; --i) {
+        for (unsigned int j = 0; j < PIXEL_WIDTH; ++j)
+            fprintf(fp, "%hhu ", image[j + PIXEL_HEIGHT*(i-1)]);
         
         fprintf(fp, "\n");
     }
@@ -251,14 +258,14 @@ int write_ascii_image_to_disk(unsigned char* image, const unsigned int WIDTH, co
 }
 
 #ifdef ENABLE_PNG
-// this function takes an image array of length WIDTH*HEIGHT
+// this function takes an image array of length PIXEL_WIDTH*PIXEL_HEIGHT
 // and produces an image file in png format of the name <filename>.txt
-int write_png_image_to_disk(unsigned char* image, const unsigned int WIDTH, const unsigned int HEIGHT,
+int write_png_image_to_disk(unsigned char* image, const unsigned int PIXEL_WIDTH, const unsigned int PIXEL_HEIGHT,
                                 const unsigned int NCOLORS, const char* const filename, const char* const palette) {
     int rc = 0;
     // an array containing pointers to every row of the image
     // rows[0] is a pointer to the start of the top most row
-    png_bytep rows[HEIGHT];
+    png_bytep rows[PIXEL_HEIGHT];
 
     // open file and initialise libpng
     FILE *fp;
@@ -293,7 +300,7 @@ int write_png_image_to_disk(unsigned char* image, const unsigned int WIDTH, cons
     png_init_io(png_write_ptr, fp);
 
     // set up all the image parameters
-    png_set_IHDR(png_write_ptr, png_info_ptr, WIDTH, HEIGHT, /*bit_depth*/ 8, PNG_COLOR_TYPE_PALETTE,
+    png_set_IHDR(png_write_ptr, png_info_ptr, PIXEL_WIDTH, PIXEL_HEIGHT, /*bit_depth*/ 8, PNG_COLOR_TYPE_PALETTE,
         PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
     // specify the color palette
@@ -315,9 +322,9 @@ int write_png_image_to_disk(unsigned char* image, const unsigned int WIDTH, cons
     // we need to construct an array of row_pointers of type png_byte which is
     // defined as unsigned char
     // now we reverse the row ordering, as libpng expects the rows from top to bottom
-    for (int row_index = 0; row_index < HEIGHT; ++row_index) {
-        // the index formula is derived from image + WIDTH*HEIGHT - row_index*WIDTH
-        rows[row_index] = image + (HEIGHT - row_index) * WIDTH;
+    for (int row_index = 0; row_index < PIXEL_HEIGHT; ++row_index) {
+        // the index formula is derived from image + PIXEL_WIDTH*PIXEL_HEIGHT - row_index*PIXEL_WIDTH
+        rows[row_index] = image + (unsigned long)(PIXEL_HEIGHT - row_index) * PIXEL_WIDTH;
     }
     png_write_image(png_write_ptr, rows);
     png_write_end(png_write_ptr, NULL);
